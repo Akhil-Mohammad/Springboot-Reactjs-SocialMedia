ed7b342d709494682a124b8cce9b0d6c026364d3
AkhilMohammad <aakhilmohammad@gmail.com>
2024-09-15 10:01:05 +0530

    Started project for social media using springboot and reactjs

---
+++ src/test/java/com/socialmedia/v/VApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia.v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
